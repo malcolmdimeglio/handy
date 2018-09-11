959c8bf026dbeec56b2be230c7afea5ad04c04cc
Malcolm DIMEGLIO <malcolm.dimeglio@gmail.com>
2018-09-11 11:20:17 -0700

    Add handy prompt colours to source

---
+++ colour.sh
@@ -0,0 +1,73 @@
+#! /bin/bash
+
+COLOR_OFF='\e[0m'       # Text Reset
+
+# Regular Colors
+BLACK='\e[0;30m'        # Black
+RED='\e[0;31m'          # Red
+GREEN='\e[0;32m'        # Green
+YELLOW='\e[0;33m'       # Yellow
+BLUE='\e[0;34m'         # Blue
+PURPLE='\e[0;35m'       # Purple
+CYAN='\e[0;36m'         # Cyan
+WHITE='\e[0;37m'        # White
+
+# Bold
+B_BLACK='\e[1;30m'       # Black
+B_RED='\e[1;31m'         # Red
+B_GREEN='\e[1;32m'       # Green
+B_YELLOW='\e[1;33m'      # Yellow
+B_BLUE='\e[1;34m'        # Blue
+B_PURPLE='\e[1;35m'      # Purple
+B_CYAN='\e[1;36m'        # Cyan
+B_WHITE='\e[1;37m'       # White
+
+# Underline
+U_BLACK='\e[4;30m'       # Black
+U_RED='\e[4;31m'         # Red
+U_GREEN='\e[4;32m'       # Green
+U_YELLOW='\e[4;33m'      # Yellow
+U_BLUE='\e[4;34m'        # Blue
+U_PURPLE='\e[4;35m'      # Purple
+U_CYAN='\e[4;36m'        # Cyan
+U_WHITE='\e[4;37m'       # White
+
+# Background
+BACK_BLACK='\e[40m'       # Black
+BACK_RED='\e[41m'         # Red
+BACK_GREEN='\e[42m'       # Green
+BACK_YELLOW='\e[43m'      # Yellow
+BACK_BLUE='\e[44m'        # Blue
+BACK_PURPLE='\e[45m'      # Purple
+BACK_CYAN='\e[46m'        # Cyan
+BACK_WHITE='\e[47m'       # White
+
+# High Intensity
+I_BLACK='\e[0;90m'       # Black
+I_RED='\e[0;91m'         # Red
+I_GREEN='\e[0;92m'       # Green
+I_YELLOW='\e[0;93m'      # Yellow
+I_BLUE='\e[0;94m'        # Blue
+I_PURPLE='\e[0;95m'      # Purple
+I_CYAN='\e[0;96m'        # Cyan
+I_WHITE='\e[0;97m'       # White
+
+# Bold High Intensity
+BI_BLACK='\e[1;90m'      # Black
+BI_RED='\e[1;91m'        # Red
+BI_GREEN='\e[1;92m'      # Green
+BI_YELLOW='\e[1;93m'     # Yellow
+BI_BLUE='\e[1;94m'       # Blue
+BI_PURPLE='\e[1;95m'     # Purple
+BI_CYAN='\e[1;96m'       # Cyan
+BI_WHITE='\e[1;97m'      # White
+
+# High Intensity backgrounds
+BACK_I_BLACK='\e[0;100m'   # Black
+BACK_I_RED='\e[0;101m'     # Red
+BACK_I_GREEN='\e[0;102m'   # Green
+BACK_I_YELLOW='\e[0;103m'  # Yellow
+BACK_I_BLUE='\e[0;104m'    # Blue
+BACK_I_PURPLE='\e[0;105m'  # Purple
+BACK_I_CYAN='\e[0;106m'    # Cyan
+BACK_I_WHITE='\e[0;107m'   # White
