1f72ca6cd06a499535ffee857eda0887441112fe
Malcolm Dimeglio <malcolm.dimeglio@gmail.com>
2019-03-01 20:58:02 -0800

    Send files over TCP and check integrity

--- TCPclient.py
+++ TCPclient.py
@@ -1,21 +1,138 @@
 #!/usr/bin/env python3
 
-import socket
+import socket, sys, subprocess, json, time
 
-HOST_IP = '127.0.0.1'  # The server's hostname or IP address
+HOST_IP = '192.168.1.21'  # The server's hostname or IP address
 SERVER_PORT = 3333        # The port used by the server
 
-tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+MODE_LISTEN="s0"
+
+MODE_TEXT="s1"
+MODE_TEXT_OK="s11"
+MODE_TEXT_NOK="s12"
+
+MODE_FILE="s2"
+MODE_FILE_OK="s21"
+MODE_FILE_NOK="s22"
+
+START_SEND_DATA="s3"
+START_SEND_DATA_OK="s30"
+SEND_DATA_SUCCESS="s31"
+SEND_DATA_FAIL="s32"
+
+SEND_TRANSFERT_END="s4"
+SEND_TRANSFERT_SUCCESS="s41"
+SEND_TRANSFERT_FAIL="s42"
+
+
+class TCPclient():
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+    def connect(self):
+        self.sock.connect((HOST_IP, SERVER_PORT))
+
+
+    def getFileInfo(self, filePath):
+        fileInfo = {'name': '',
+                    'md5sum': ''}
+
+        filename = filePath.strip().split('/')[-1]
+        cmd = 'md5sum ' + filePath + ' | cut -d\' \' -f1'
+        md5 = subprocess.run(cmd,shell=True, check=True, stdout=subprocess.PIPE).stdout.decode().strip()
+
+        fileInfo['name'] = filename
+        fileInfo['md5sum'] = md5
+
+        return fileInfo
+
+
+    def sendData (self, data):
+        """
+        Send data to the client using socket 'sock'
+        """
+        try :
+            self.sock.sendall(data)
+        except socket.error as e :
+            print("Error sending data: %s" %e)
+            sys.exit(1)
+
+    def recvData (self, size=1024):
+        """
+        Receive data of size 'size' from the client using socket 'sock'
+        """
+        try :
+            data = self.sock.recv(size)
+        except socket.error as e :
+            print("Error receiving data: %s" %e)
+            sys.exit(1)
+
+        return data
+
+    def sendFile (self, fileInfo):
+        """
+        Function to read and send the file 'filename'
+        to the client using the socket 'sock'
+        """
+        try :
+            fd = open(fileInfo['name'], 'rb')
+            self.sendData(MODE_FILE.encode())  # Tell the server we want to send a file
+            if self.recvData().decode() == MODE_FILE_OK:  # The server is in file reception mode
+                print("Server in file reception mode")
+                self.sendData(START_SEND_DATA.encode())  # Tell the server we want to send data
+                if self.recvData().decode() == START_SEND_DATA_OK: # The server is ready to receive data
+                    print("Server Ready to receive data")
+                    self.sendData(json.dumps(fileInfo_dict).encode())  # Send data
+                    if self.recvData().decode() == SEND_DATA_SUCCESS:
+                        print("File info received by the server")
+                    else:
+                        print("ERROR, data has not been received properly")
+                        return
+                else:
+                    print("ERROR, the server cannot receive data at the moment")
+                    return
+            else:
+                print ("ERROR, cannot send file, check with server")
+                return
+        except IOError :
+            print("---File Not Present---")
+            return
+
+        print('Sending file: ' + fileInfo['name'])
+        read_data = fd.read(4096)
+        while (read_data ):
+            #print('sending data...')
+            self.sendData(read_data)
+            #print('Sent ',repr(read_data))
+            ack = self.recvData()
+            if ack.decode() == SEND_DATA_FAIL:
+                print("ERROR, FILE TRANSFERT FAILED")
+                return
+            else:
+                print("Transfert...")
+            read_data = fd.read(4096)
+        fd.close()
+
+        self.sendData(SEND_TRANSFERT_END.encode())
+        if self.recvData().decode() == SEND_TRANSFERT_SUCCESS:
+            print("SUCCESS, File transfered successfuly")
+        else:
+            print("ERROR, File transfer failed")
+
+    def close(self):
+        self.sock.close()
+
+
+tcp_client = TCPclient()
 
 try:
     # Establish connection to TCP server and exchange data
-    tcp_client.connect((HOST_IP, SERVER_PORT))
-    message='Hello, world'
-    tcp_client.sendall(message.encode())
+    tcp_client.connect()
+    # tcp_client.sendData("Hello World".encode())
+    print("Extracting file info...")
+    fileInfo_dict = tcp_client.getFileInfo('test.txt')
+    tcp_client.sendFile(fileInfo_dict)
 
-    # Read data from the TCP server and close the connection
-    received = tcp_client.recv(1024).decode()
 finally:
     tcp_client.close()
-
-print(received)
--- TCPserver.py
+++ TCPserver.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python3
 
-import socketserver
+from pathlib import Path
+import socketserver, subprocess, json
 
-HOST_IP = '127.0.0.1'  # The server's hostname or IP address
+HOST_IP = '192.168.1.21'  # The server's hostname or IP address
 SERVER_PORT = 3333        # The port used by the server
+HOME = str(Path.home()) + '/'
+
+MODE_LISTEN="s0"
+
+MODE_TEXT="s1"
+MODE_TEXT_OK="s11"
+MODE_TEXT_NOK="s12"
+
+MODE_FILE="s2"
+MODE_FILE_OK="s21"
+MODE_FILE_NOK="s22"
+
+START_SEND_DATA="s3"
+START_SEND_DATA_OK="s30"
+SEND_DATA_SUCCESS="s31"
+SEND_DATA_FAIL="s32"
+
+SEND_TRANSFERT_END="s4"
+SEND_TRANSFERT_SUCCESS="s41"
+SEND_TRANSFERT_FAIL="s42"
+
+
 
 class Handler_TCPServer(socketserver.BaseRequestHandler):
     """
@@ -14,12 +37,78 @@ class Handler_TCPServer(socketserver.BaseRequestHandler):
 
     """
 
+    def fileRecv (self, fileInfo):
+        """
+        Function to receive the file 'filename' from the server
+        using the socket 'sock' and write in on the client side
+        """
+        print("---File Present---")
+
+        fileInfo = json.loads(fileInfo)
+        # Read the md5sum from the file to receive
+        md5sumRemote = fileInfo["md5sum"]
+        filename = fileInfo["name"]
+
+        print("filename received: " + filename)
+        print("md5sum received: " + md5sumRemote)
+
+        destination_file = HOME + 'received_' + filename
+
+        with open(destination_file, 'wb') as f:
+            print('Destination file created')
+            print('Receiving Data...')
+            while True:
+                #print('receiving data...')
+                data = self.request.recv(4096)
+                #print("data = %s", (data))
+
+                if not data or data == 'EOF' or data.decode() == SEND_TRANSFERT_END:
+                    break
+
+                # write data to a file
+                f.write(data)
+                self.request.send(SEND_DATA_SUCCESS.encode())
+        f.close()
+
+        # Get md5sum from the received file
+        cmd = 'md5sum ' + destination_file + ' | cut -d\' \' -f1'
+        md5sumLocal = subprocess.run(cmd,shell=True, check=True, stdout=subprocess.PIPE).stdout.decode().strip()
+
+        if md5sumLocal == md5sumRemote:
+            print("SUCCESS: File transfered: " + filename)
+            self.request.send(SEND_TRANSFERT_SUCCESS.encode())
+        else:
+            print('File ' + filename + ' is corrupted')
+            print('Remote: ' + md5sumRemote)
+            print('Local:  ' + md5sumLocal)
+            self.request.send(SEND_TRANSFERT_FAIL.encode())
+
     def handle(self):
         # self.request - TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} sent: {}".format(self.client_address[0], self.data.decode()))
+        self.data = self.request.recv(1024)
+
+        if self.data.decode() == MODE_FILE:
+            self.request.sendall(MODE_FILE_OK.encode())
+            print("Accepting File reception mode")
+
+            self.data = self.request.recv(1024)
+            if self.data.decode() == START_SEND_DATA:
+                self.request.sendall(START_SEND_DATA_OK.encode())
+                print("Accepting incoming data")
+
+                fileInfo = self.request.recv(1024)
+                self.request.sendall(SEND_DATA_SUCCESS.encode())
+
+                self.fileRecv(fileInfo.decode())
+        elif self.data.decode() == MODE_TEXT or self.data.decode() == START_SEND_DATA:
+            # print("---File Not Present---")
+            print("{} sent: {}".format(self.client_address[0], self.data.decode()))
+        else:
+            print("ERROR, Unsuported transfert mode")
+
         # just send back ACK for data arrival confirmation
-        self.request.sendall("ACK from TCP Server".encode())
+        self.request.sendall("Server is listening...".encode())
+        print("Listening...")
 
 if __name__ == "__main__":
     # Init the TCP server object, bind it to the localhost on 9999 port
@@ -27,4 +116,5 @@ if __name__ == "__main__":
 
     # Activate the TCP server.
     # To abort the TCP server, press Ctrl-C.
+    print("Listening...")
     tcp_server.serve_forever()
